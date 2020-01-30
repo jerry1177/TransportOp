c1807fb3fc550870afc57d5e4119f763ea286b62
jerry1177 <jerrymaldonadom@gmail.com>
2020-01-30 14:29:47 -0800

    Added very basic interface for testing communication to backend server.

--- app/src/main/java/com/example/transportop/MainActivity.java
+++ app/src/main/java/com/example/transportop/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.transportop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.BundleCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String database = BuildConfig.Backend;
+        // create the request queue
+        queue = Volley.newRequestQueue(this);
+    }
+
+    public void sendRequest(View view) {
+        final TextView textView = (TextView) findViewById(R.id.HttpView);
+
+
+        String url = BuildConfig.Backend + "/api/user/read.php";
+
+
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        textView.setText("Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        textView.setText("Error! Response!");
+                    }
+                });
+
+        //textView.setText(url);
+        // Request a string response from the provided URL.
+        /*
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        */
+        queue.add(jsonObjectRequest);
     }
 }
